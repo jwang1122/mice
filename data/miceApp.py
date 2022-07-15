c7d992a75b934a8091b70e16e2cbdec944e4899b
jwang1122 <jwang1122@gmail.com>
2022-07-14 19:22:22 -0500

    use localhost

--- data/miceApp.py
+++ data/miceApp.py
@@ -122,4 +122,4 @@ def update_user(mouse_id):
 
 
 if __name__ == '__main__':
-    app.run(host="192.168.3.19", port=5000)
+    app.run(host="localhost", port=5000)
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
   //192.168.3.19
-const url = 'http://192.168.3.19:5000'
+const url = 'http://localhost:5000'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
