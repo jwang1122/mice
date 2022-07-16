c04cc8cefcb842c322cd6907a79eeed0b62dac7d
jwang1122 <jwang1122@gmail.com>
2022-07-16 10:43:31 -0500

    add cages management

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import ResponsiveAppBar from './components/Nav'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AddMouse from './components/AddMouse'
+import AddCage from './components/AddCage'
 import AddBreeding from './components/AddBreeding'
 import Mice from './Mice'
+import Cages from './Cages.js'
 import PdfReport from './components/PdfReport'
 
 const App = props => {
@@ -14,7 +16,9 @@ const App = props => {
       <Routes>
         <Route path="/" element={<Mice url={url} />} />
         <Route path="/home" element={<Mice url={url} />} />
+        <Route path="/cages" element={<Cages url={url} />} />
         <Route path="/addmouse" element={<AddMouse url={url} />} />
+        <Route path="/addcage" element={<AddCage url={url} />} />
         <Route path="/addbreeding" element={<AddBreeding url={url} />} />
         <Route path="/report" element={<PdfReport url={url} />} />
       </Routes>
