2c7725ce2bc747199fb908e87c3d523959083221
jwang1122 <jwang1122@gmail.com>
2022-07-23 05:30:10 -0500

    fake reminder

--- src/components/actions/PairingReminder.js
+++ src/components/actions/PairingReminder.js
@@ -1,6 +1,21 @@
-const PairingReminder = (props) =>{
+import React from 'react'
+
+const PairingReminder = () => {
     return (
+        <>
         <h1>Pairing Reminder will be coming soon.</h1>
+        <table border="1" cellPadding="15">
+            <tr>
+                <th>Cage</th><th>21 days Count Down</th>
+            </tr>
+            <tr>
+                <td>A01</td><td>13 days left.</td>
+            </tr>
+            <tr>
+                <td>EB15+++</td><td>2 days left.</td>
+            </tr>
+        </table>
+        </>
     )
 }
 
